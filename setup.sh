8fdaedbf04319cf4d215eda0d3cc5d9c2308edd9
snapekk <126613007+snapekk@users.noreply.github.com>
2026-05-18 17:05:10 -0300

    Update setup.sh
    im tired changing that thing

--- setup.sh
+++ setup.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
-
 VERSION="1.20.4"
-RAM="6G"
 SERVER_DIR="$HOME/minecraft_server"
 GITHUB_USER="snapekk"
 REPO_NAME="mine-server"
@@ -12,11 +9,13 @@ if [ -n "$PREFIX" ] && [[ "$PREFIX" == *"/com.termux"* ]]; then
     echo "-> Installing Termux dependencies..."
     pkg update -y
     pkg install openjdk-17 wget curl git screen -y
-    JAVA_FLAGS="-XX:+UseZGC -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+PerfDisableSharedMem"
+    RAM="4G"
+    JAVA_FLAGS="-XX:+UseG1GC"
 else
     echo "-> Installing Ubuntu dependencies..."
     sudo apt update && sudo apt upgrade -y
     sudo apt install openjdk-21-jre-headless screen wget curl git unzip -y
+    RAM="6G"
     JAVA_FLAGS="-XX:+UseZGC -XX:+ZGenerational -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+PerfDisableSharedMem"
 fi
 
@@ -39,20 +38,19 @@ EOF
 
 echo "-> Downloading mods folder..."
 git clone https://github.com/$GITHUB_USER/$REPO_NAME.git temp_repo
-
 cp -r temp_repo/mods ./
 rm -rf temp_repo fabric-installer.jar
 
 mkdir -p config
 cat <<EOF > config/c2me.toml
 [threadedWorldGen]
-    allowThreadedFeatures = "true"
-    reduceLockRadius = "true"
+allowThreadedFeatures = true
+reduceLockRadius = true
 [asyncScheduling]
-    enabled = "true"
+enabled = true
 [ioSystem]
-    replaceImpl = "true"
-    asyncMACThreads = 3
+replaceImpl = true
+asyncMACThreads = 3
 EOF
 
 echo "-> Creating start script with ZGC..."
